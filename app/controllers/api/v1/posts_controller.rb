276d93b95213f869df1d548052de36aae575833e
mattbmoorin <tzaratape@gmail.com>
2021-03-11 22:48:10 -0600

    built controller

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,37 @@
+class Api::V1::PostsController < ApplicationController
+    def index
+        @posts = Post.all
+
+        render json: @posts, status: 200
+    end
+
+    def show
+        @post = Post.find(params[:id])
+        
+        render json: @post. status: 200
+    end
+
+    def create
+        @post = Post.create(post_params)
+
+        render json: @post. status: 200
+    end
+
+    def update
+        @post = Post.find(post_params)
+        @post.update(post_params)
+        render json: @post. status: 200
+    end
+
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+
+        render json: {postId: @post.id}
+    end
+    private
+
+    def post_params
+        params.require(:post).permit(:body)
+    end
+end
